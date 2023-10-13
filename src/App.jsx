4f2c1c15b874b2cb65b3f95cd7ad26ae41210d3e
Jbeelerer <91538755+Jbeelerer@users.noreply.github.com>
2023-10-14 00:06:00 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import React from "react";
+import P5Sketch from "./P5Sketch";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="sketch">
+          <>
+            <P5Sketch />
+          </>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
